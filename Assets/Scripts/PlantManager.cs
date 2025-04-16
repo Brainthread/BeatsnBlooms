b134e7dbf2750091ebd29c66cc15459a0cab8824
unknown <keidukebergman@gmail.com>
2025-04-16 14:23:44 +0200

    Implement plant grid spawn positions

--- Assets/Scripts/PlantManager.cs
+++ Assets/Scripts/PlantManager.cs
@@ -3,10 +3,17 @@ using UnityEngine;
 public class PlantManager : MonoBehaviour
 {
     [SerializeField] private Plant[] plants;
+    private int plantPosition = 4;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         EventHandler.current.onPlantActivation += OnPlantActivation;
+        for(int i = 0; i < plants.Length; i++)
+        {
+            if (!plants[i])
+                continue;
+            plants[i].transform.position = GridManager.current.GridPositionToWorldPosition(new Vector2(plantPosition, i));
+        }
     }
 
     // Update is called once per frame
